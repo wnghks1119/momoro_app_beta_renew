56abe493ed361a5413d08a0d74c6a7d700529b3d
Juhwan <wnghks1119@naver.com>
2023-08-01 09:05:51 +0900

    commit

--- lib/data_manage_component/db_helper.dart
+++ lib/data_manage_component/db_helper.dart
@@ -2,59 +2,78 @@ import 'package:sqflite/sqflite.dart' as sql;
 
 // 테이블 생성
 class SQLHelper {
-  static Future<void> createTables(sql.Database database) async{
+  static Future<void> createTables(sql.Database database) async {
     await database.execute("""CREATE TABLE data(
-      id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-      title TEXT,
-      desc TEXT,
+      id INTEGER PRIMARY KEY NOT NULL,
+      rating REAL NOT NULL,
+      thanksDesc TEXT,
+      reliefDesc TEXT,
+      goodDesc TEXT,
       createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
     )""");
-  } // createdAt 이거는 딱히 없어도 될 듯?
+  } // 아이디, 하루 점수, 감사한 일 글, 다행인 일 글, 잘한 일 글, 해당일 날짜
 
+  /*
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  title TEXT,
+  desc TEXT,
+  createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+  */
 
-  static Future<sql.Database> db() async{
+  // 최초로 앱 실행하여 데이터 아무것도 없는 경우에 데이터베이스 생성을 위한 함수
+  static Future<sql.Database> db() async {
     return await sql.openDatabase("database_name.db", version: 1,
-        onCreate: (sql.Database database, int version) async{
-          await createTables(database);
-        });
+        onCreate: (sql.Database database, int version) async {
+      await createTables(database);
+    });
   }
 
-  // 데이터 추가
-  static Future<int> createData(String title, String? desc) async {
+  // InputListFirstScreen에서 '저장' 버튼 누를 경우 데이터가 데이터베이스에 저장되도록 하는 함수
+  static Future<int> createData(double rating, String? thanksDesc,
+      String? reliefDesc, String? goodDesc, DateTime createdAt) async {
     final db = await SQLHelper.db();
 
-    final data = {'title': title, 'desc': desc};
-    final id = await db.insert('data', data,conflictAlgorithm: sql.ConflictAlgorithm.replace);
+    final data = {
+      'rating': rating,
+      'thanksDesc': thanksDesc,
+      'reliefDesc': reliefDesc,
+      'goodDesc': goodDesc,
+      'createdAt': createdAt,
+    };
+    final id = await db.insert('data', data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
 
     return id;
   }
 
+  // 생성한 모든 데이터(레코드)를 저장해두는 데이터베이스
   static Future<List<Map<String, dynamic>>> getAllData() async {
     final db = await SQLHelper.db();
     return db.query('data', orderBy: 'id');
   }
 
-  static Future<List<Map<String, dynamic>>> getSingleData(int id) async{
+  // 전체 데이터 중 특정 월에 대한 데이터만 추출해주는 함수 (where 조건 createdAt 이용해서 범위로 조절)
+  static Future<List<Map<String, dynamic>>> getMonthData(DateTime createdAt) async {
     final db = await SQLHelper.db();
-    return db.query('data', where:'id = ?', whereArgs: [id], limit: 1);
+    return db.query('data', where: 'createdAt = ?', whereArgs: [createdAt], limit: 1);
+  }
+  // 전체 데이터 중 특정 일에 대한 데이터만 추출해주는 함수 (where 조건 createdAt 이용해서 캘린더에서 선택한 일에 대한 데이터 추출)
+  static Future<List<Map<String, dynamic>>> getDayData(DateTime createdAt) async {
+    final db = await SQLHelper.db();
+    return db.query('data', where: 'createdAt = ?', whereArgs: [createdAt], limit: 1);
   }
 
-  static Future<int> updateData(int id, String title, String? desc) async {
+  // InputListLaterScreen에서 '텍스트 수정 시' 수정내용 반영될 수 있도록 하기
+  static Future<int> updateDescData(DateTime createdAt, String? thanksDesc,
+      String? reliefDesc, String? goodDesc) async {
     final db = await SQLHelper.db();
     final data = {
-      'title' : title,
-      'desc' : desc,
-      'createdAt' : DateTime.now().toString()
+      'thanksDesc': thanksDesc,
+      'reliefDesc': reliefDesc,
+      'goodDesc': goodDesc,
     };
     final result =
-    await db.update('data', data, where: 'id = ?', whereArgs: [id]);
+    await db.update('data', data, where: 'createdAt = ?', whereArgs: [createdAt]);
     return result;
-  }
-
-  static Future<void> deleteData(int id) async {
-    final db = await SQLHelper.db();
-    try {
-      await db.delete('data', where: 'id = ?', whereArgs: [id]);
-    } catch (e) {}
   }
 }
